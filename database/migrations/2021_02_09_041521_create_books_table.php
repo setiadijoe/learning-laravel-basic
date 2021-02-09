9d6a78bab335449a50fd7b593f4e278b42f46613
Yonathan Setiadi <setiadi_y56@yahoo.co.id>
2021-02-09 13:27:38 +0700

    create migrations

---
+++ database/migrations/2021_02_09_041521_create_books_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('title')->index();
+            $table->string('description')->nullable();
+            $table->string('status')->nullable()->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
